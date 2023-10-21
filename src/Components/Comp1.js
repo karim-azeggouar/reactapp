3bb5a48ff47f77e85b1395185bb3c70155719233
Azeggouar <k.azeggouar@gmail.com>
2023-10-21 12:31:11 +0000

    4 commit

--- src/Components/Comp1.js
+++ src/Components/Comp1.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Comp1() {
+
+const [toogle,setToogle]=useState(true);
+
+
+const elmt= toogle? '<h1>Hello True</h1>':'<h1>Hi false</h1>'
+
+
   return (
     <div>
       <h1>   Hello From GIT HUB2</h1>
+      {elmt}
+
+      <input type="button"   onClick={setToogle(!toogle)} value="toogle" />
     </div>
   )
 }
