2941c22061bf27757ddefe1dfcdcc316c2ff0272
Azeggouar <k.azeggouar@gmail.com>
2023-10-21 12:37:03 +0000

    5 commit

--- src/Components/Comp1.js
+++ src/Components/Comp1.js
@@ -5,15 +5,15 @@ export default function Comp1() {
 const [toogle,setToogle]=useState(true);
 
 
-const elmt= toogle? '<h1>Hello True</h1>':'<h1>Hi false</h1>'
+const elmt= toogle ? <h1>Hello True</h1>:<h1>Hi false</h1>
 
 
   return (
     <div>
       <h1>   Hello From GIT HUB2</h1>
-      {elmt}
+              {elmt}
 
-      <input type="button"   onClick={setToogle(!toogle)} value="toogle" />
+              <input type="button"   onClick={()=>setToogle(!toogle)} value="toogle" />
     </div>
   )
 }
